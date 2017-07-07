4c1fc17c084a8e583a46f83df76105504c3ee1dc
Javid <javid@OBLAP-46>
2017-07-07 20:31:53 +0530

    commit after format

--- modules/UserModule/Model/User.js
+++ modules/UserModule/Model/User.js
@@ -20,30 +20,30 @@ class User{
     }
 	insert(formData,response,MongoCon) {
         this.connection(function(userModel){
-        var userData = new userModel({userName:formData.username,password:formData.password,email:formData.email});
-        userData.save(function(err,userData){
-            if(err){
-                console.log(err);
-            }else{
-                response.redirect('/');
-            }
+            var userData = new userModel({userName:formData.username,password:formData.password,email:formData.email});
+            userData.save(function(err,userData){
+                if(err){
+                    console.log(err);
+                }else{
+                    response.redirect('/');
+                }
+            });
         });
-        })
        
 	}
     checklogin(formData,response,MongoCon) {
         this.connection(function(userModel){
-        userModel.findOne({$or:[{"userName":formData.email},{"email": formData.email}]},function(err,data){
-			if(err){
-				throw err;
-			}else{
-                if(data.password==formData.password){ 
-
+            userModel.findOne({$or:[{"userName":formData.email},{"email": formData.email}]},function(err,data){
+                if(err){
+                    throw err;
                 }else{
+                    if(data.password==formData.password){ 
 
+                    }else{
+
+                    }
                 }
             }
-        }
         )}
     )};
 }
